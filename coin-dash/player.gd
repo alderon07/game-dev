fb891414e49a70128206b8c448ee2a74e4173f94
Naqi <naqihaider07@gmail.com>
2026-07-22 22:43:54 -0400

    updated player scene

--- coin-dash/player.gd
+++ coin-dash/player.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+signal pickup
+signal hurt
+
 @export var speed: int = 350
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
@@ -34,3 +37,7 @@ func _process(delta: float) -> void:
 func die():
 	sprite.animation = "hurt"
 	set_process(false)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
